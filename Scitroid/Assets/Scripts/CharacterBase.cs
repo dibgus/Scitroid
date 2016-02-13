02c42e000da776480aef82c2dced00ab8e15385b
Sam <smmauck@gmail.com>
2016-02-13 14:14:28 -0700

    Add character base class

---
+++ Scitroid/Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class CharacterBase : MonoBehaviour {
+
+    abstract int characterHealth;
+    abstract int characterEnergy;
+    abstract int characterSpeed;
+    abstract int baseAttackDamage;
+
+	// Use this for initialization
+	abstract void Start() {}
+	
+	// Update is called once per frame
+	abstract void Update() {}
+
+    abstract void Move() {}
+
+    int getHealth() { return characterHealth; }
+
+    int getEnergy() { return characterEnergy; }
+
+    int getSpeed() { return characterSpeed; }
+
+    int getBaseAttackDamage() { return baseAttackDamage; }
+
+
+}
