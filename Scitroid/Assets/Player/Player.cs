a56725576cad186405d82e849d9e5f406ac6f9e1
jmatney2 <jmatney2@cherrycreekschools.org>
2016-02-13 21:11:04 -0700

    Punching functionality!

--- Scitroid/Assets/Player/Player.cs
+++ Scitroid/Assets/Player/Player.cs
@@ -8,10 +8,12 @@ public class Player : MonoBehaviour {
     bool RightFacing;
     public Sprite[] walking;
     public Sprite[] ghostSprites;
-    public SpriteRenderer thisSprite;
+    public Sprite[] punchingSprites;
+    SpriteRenderer thisSprite;
     
     //Movement
     bool HasJumped = false;
+    bool IsMoving = false;
     public float speed;
 
     //Stats
@@ -35,51 +37,91 @@ public class Player : MonoBehaviour {
     private GameObject instantiatedBurst;
     public GameObject blinkBurst;
 
-	// Use this for initializations
+    //Attacks
+    bool IsPunching = false;
 
-	void Start () {
+    //Camera
+    Camera cam;
+    float minY = 0, minX = 0, maxY = 0, maxX = 0;
+
+    // Use this for initializations
+
+    void Start () {
+        thisSprite = GetComponent<SpriteRenderer>();
         thisSprite.sprite = walking[0];
+        CamSetup();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    if(Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += Vector3.right*0.01f;
+            transform.position += Vector3.right * 0.01f;
             RightFacing = true;
-            thisSprite.sprite = walking[SpriteNum];
+            if (!IsPunching) thisSprite.sprite = walking[SpriteNum];
             Vector3 newScale = this.transform.localScale;
             newScale.x = 1;
             this.transform.localScale = newScale;
+            IsMoving = true;
         }
         else if(Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position -= Vector3.right * 0.01f;
             RightFacing = false;
-            thisSprite.sprite = walking[SpriteNum];
+            if (!IsPunching) thisSprite.sprite = walking[SpriteNum];
             Vector3 newScale = this.transform.localScale;
             newScale.x = -1;
             this.transform.localScale = newScale;
-            
+            IsMoving = true;
         }
         else
         {
-            counter = 0;
-            if (RightFacing) thisSprite.sprite = walking[1];
-            else thisSprite.sprite = walking[1];
+            IsMoving = false;
+            if (!IsPunching)
+            {
+                if (RightFacing) thisSprite.sprite = walking[1];
+                else thisSprite.sprite = walking[1];
+            }
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
             if(!HasJumped)GetComponent<Rigidbody2D>().velocity = Vector2.up*3;
             HasJumped = true;
         }
-        counter += 0.05f;
-        if(counter > 0.5f)
+        if (Input.GetKey(KeyCode.Q))
         {
-            counter = 0;
-            SpriteNum += 1;
-            if (SpriteNum >= walking.Length) SpriteNum = 0;
-            thisSprite.sprite = walking[SpriteNum];
+            if (!IsPunching)
+            {
+                counter = 0;
+                SpriteNum = 0;
+                thisSprite.sprite = punchingSprites[0];
+            }
+            IsPunching = true;
+        }
+        counter += Time.deltaTime;
+        if (IsPunching)
+        {
+            if (counter > 0.1f)
+            {
+                counter = 0;
+                SpriteNum++;
+                if (SpriteNum >= punchingSprites.Length)
+                {
+                    IsPunching = false;
+                    SpriteNum = 0;
+                }
+                thisSprite.sprite = punchingSprites[SpriteNum];
+            }
+        }
+        else
+        {
+            if (counter > 0.2f && IsMoving)
+            {
+                counter = 0;
+                SpriteNum += 1;
+                if (SpriteNum >= walking.Length) SpriteNum = 0;
+                thisSprite.sprite = walking[SpriteNum];
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.E))
@@ -104,6 +146,7 @@ public class Player : MonoBehaviour {
 
                 blinkGhost = true;
             }
+            CamFollow();
         }
 
         if (instantiatedGhost != null)
@@ -142,10 +185,54 @@ public class Player : MonoBehaviour {
             instantiatedPlasma = (GameObject) Instantiate(plasma, this.transform.position, Quaternion.identity);
             shotDelay = plasmaDelay;
         }
+        CamFollow();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         HasJumped = false;
+    }
+
+    void CamSetup()
+    {
+        cam = Camera.main;
+        GameObject[] allObjects = (GameObject[])GameObject.FindObjectsOfType(typeof(GameObject));
+        foreach (GameObject g in allObjects)
+        {
+            if (g.GetComponent<SpriteRenderer>() == null) continue;
+            float right = g.transform.position.x + g.GetComponent<SpriteRenderer>().sprite.bounds.size.x / 2;
+            float left = g.transform.position.x - g.GetComponent<SpriteRenderer>().sprite.bounds.size.x / 2;
+            float top = g.transform.position.y + g.GetComponent<SpriteRenderer>().sprite.bounds.size.y / 2;
+            float bottom = g.transform.position.y - g.GetComponent<SpriteRenderer>().sprite.bounds.size.y / 2;
+            maxX = right > maxX ? right : maxX;
+            minX = left < minX ? left : minX;
+            maxY = top > maxY ? top : maxY;
+            minY = bottom < minY ? bottom : minY;
+        }
+        //cam = GetComponent<Camera>();
+    }
+
+    void CamFollow()
+    {
+        float boundsY = cam.orthographicSize;
+        float boundsX = cam.orthographicSize * Screen.width / Screen.height;
+        //float SetX = player.transform.position.x + boundsX < maxX && player.transform.position.x-boundsX > minX ? player.transform.position.x : transform.position.x;
+        //float SetY = player.transform.position.y + boundsY < maxY && player.transform.position.y-boundsY > minY ? player.transform.position.y : transform.position.y;
+        float SetX;
+        float SetY;
+        if (transform.position.x + boundsX < maxX && transform.position.x - boundsX > minX) SetX = transform.position.x;
+        else
+        {
+            if (transform.position.x + boundsX >= maxX) SetX = maxX - boundsX;
+            else SetX = minX + boundsX;
+        }
+        if (transform.position.y + boundsY < maxY && transform.position.y - boundsY > minY) SetY = transform.position.y;
+        else
+        {
+            if (transform.position.y + boundsY >= maxY) SetY = maxY - boundsY;
+            else SetY = minY + boundsY;
+        }
+        //print(boundsX + "  ::  " + maxX +" , " + minX + "  ::  " + (player.transform.position.x + boundsX) + " , " + (player.transform.position.x-boundsX));
+        cam.transform.position = new Vector3(SetX, SetY, -10);
     }
 }
