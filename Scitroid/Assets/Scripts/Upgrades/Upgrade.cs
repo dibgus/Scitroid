16d8a013fa2c388db71f45a082685330b5771da8
garrm101 <garrm101@hotmail.com>
2016-02-14 02:56:25 -0700

    Basic Upgrade Prefab/Sprite/Script

---
+++ Scitroid/Assets/Scripts/Upgrades/Upgrade.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Upgrade : MonoBehaviour {
+    public int upgradeID;
+    public Sprite[] sprites;
+
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpriteRenderer>().sprite = sprites[upgradeID];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (upgradeID == 0)
+            {
+                collision.GetComponent<Player>().plasmaUpgrade = true;
+            }
+
+            if (upgradeID == 1)
+            {
+                Destroy(collision.gameObject);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
