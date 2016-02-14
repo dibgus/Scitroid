85cc07064f8447ec1efe4f02a18c8ee066b3eb38
Tyler Tokumoto <Tyler Tokumoto>
2016-02-13 23:09:44 -0700

    IF (TYLER == GOD) COMMIT2GIT.JPG

--- Scitroid/Assets/Scripts/Enemy.cs
+++ Scitroid/Assets/Scripts/Enemy.cs
@@ -3,44 +3,46 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
-    public Sprite[] enemyRight;
-    public Sprite[] enemyLeft;
+    public Sprite[] enemyWalking;
     public SpriteRenderer basicEnemy;
+    int spriteCounter = 0;
+    float timer = 0;
+    Vector3 scale;
 
     int health = 100;
     int damage = 10;
-    int jumpLayer = 1 << 8;
-    int edgeLayer = 1 << 9;
-    public bool right;
+    bool right;
+    bool isJumping;
 
 	// Use this for initialization
 	void Start () {
-	    
+        scale = transform.lossyScale;
+        right = false;
+        basicEnemy.sprite = enemyWalking[0];
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Move();
+        
         Jump();
     }
 
     void Move()
     {
-        Vector2 originLeft = new Vector2(transform.position.x - 0.1f, transform.position.y);
-        Vector2 originRight = new Vector2(transform.position.x + 0.1f, transform.position.y);
+        Vector2 originLeft = new Vector2(transform.position.x - .09f, transform.position.y);
+        Vector2 originRight = new Vector2(transform.position.x + .09f, transform.position.y);
 
-        RaycastHit2D hitLeft = Physics2D.Raycast(originLeft, Vector2.down, Mathf.Infinity);
-        RaycastHit2D hitRight = Physics2D.Raycast(originRight, Vector2.down, Mathf.Infinity);
+        RaycastHit2D hitLeft = Physics2D.Raycast(originLeft, Vector2.down);
+        RaycastHit2D hitRight = Physics2D.Raycast(originRight, Vector2.down);
 
-        if (hitRight.collider == null)
+        if (hitRight.collider == null && right)
         {
-            transform.position += Vector3.left * .025f;
             right = false;
         }
 
-        if (hitLeft.collider == null)
+        if (hitLeft.collider == null && !right)
         {
-            transform.position += Vector3.right * .011f;
             right = true;
         }
 
@@ -53,18 +55,55 @@ public class Enemy : MonoBehaviour {
         {
             transform.position += Vector3.right * .011f;
         }
+
+        timer += Time.deltaTime;
+
+        if (timer > 0.2f)
+        {
+            spriteCounter++;
+            timer = 0;
+
+            if (spriteCounter >= enemyWalking.Length)
+            {
+                spriteCounter = 0;
+            }
+
+            basicEnemy.sprite = enemyWalking[spriteCounter];
+
+            if (right)
+            {
+                basicEnemy.transform.localScale = scale;
+            }
+
+            else
+            {
+                basicEnemy.transform.localScale = new Vector3(-scale.x, scale.y, scale.z);
+            }
+        }
     }
     
     void Jump()
     {
-        Vector2 originUp = new Vector2(transform.position.x, transform.position.y + 0.1f);
+        Vector2 originUp = new Vector2(transform.position.x, transform.position.y);
 
         RaycastHit2D hitLeft = Physics2D.Raycast(originUp, Vector2.left);
         RaycastHit2D hitRight = Physics2D.Raycast(originUp, Vector2.right);
 
-        if (hitRight.collider != null && hitRight.distance <= 0.25f || hitLeft.collider != null && hitLeft.distance <= 0.25f)
+        if ((hitRight.collider != null && hitRight.distance <= basicEnemy.sprite.bounds.size.x / 2 + .1) || (hitLeft.collider != null && hitLeft.distance <= basicEnemy.sprite.bounds.size.x / 2 + .1))
         {
             GetComponent<Rigidbody2D>().velocity = Vector2.up * 3;
         }
+    }
+
+    bool isInAir()
+    {
+        RaycastHit2D hitDown = Physics2D.Raycast(transform.position, Vector2.down);
+
+        if (hitDown.distance > basicEnemy.sprite.bounds.size.y / 2)
+        {
+            return true;
+        }
+
+        return false;
     }
 }
