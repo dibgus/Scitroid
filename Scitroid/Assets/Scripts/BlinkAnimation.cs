e6ab8d2361aac0556098049b9e30820bc1310136
Sam <smmauck@gmail.com>
2016-02-13 19:13:11 -0700

    Add blink animation

---
+++ Scitroid/Assets/Scripts/BlinkAnimation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlinkAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (this.GetComponent<SpriteRenderer>().color.a <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        Color color = this.GetComponent<SpriteRenderer>().color;
+        color.a -= 0.05f;
+        this.GetComponent<SpriteRenderer>().color = color;
+
+	}
+}
