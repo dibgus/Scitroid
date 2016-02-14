906a9bf59addce0bff73ec87788e33fe4957c3f5
garrm101 <garrm101@hotmail.com>
2016-02-14 08:14:13 -0700

    Added Sword

--- Scitroid/Assets/Scripts/Upgrades/Upgrade.cs
+++ Scitroid/Assets/Scripts/Upgrades/Upgrade.cs
@@ -8,7 +8,7 @@ public class Upgrade : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        GetComponent<SpriteRenderer>().sprite = sprites[0];
+        //GetComponent<SpriteRenderer>().sprite = sprites[0];
 	}
 	
 	// Update is called once per frame
